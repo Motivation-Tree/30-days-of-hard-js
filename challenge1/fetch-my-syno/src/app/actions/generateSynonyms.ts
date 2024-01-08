840a1358a8ca05e66ea9eccb652f697d07e63ff8
Awais alwaisy <awaisalwaisy@gmail.com>
2024-01-08 21:16:26 +0500

    fix(app): challenge1 fix api response
    Change response to json
    
    Closes: challenge1

--- challenge1/fetch-my-syno/src/app/actions/generateSynonyms.ts
+++ challenge1/fetch-my-syno/src/app/actions/generateSynonyms.ts
@@ -35,7 +35,13 @@ export async function generateSynonyms(word: string) {
     const generatedContent = await response.json();
     const text = generatedContent.candidates[0].content.parts[0].text;
 
-    return extractJson(text) as WordResponse;
+    console.log(JSON.parse(text));
+
+    if (JSON.parse(text).types.length === 1) {
+      return JSON.parse(text) as WordResponse;
+    } else {
+      return extractJson(text) as WordResponse;
+    }
   } catch (error) {
     console.error("Error generating story:", error);
   }
