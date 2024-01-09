fbf9d2785ef4891ceff969088ba7f90886ceea90
Muthukumaran Ganapathy <muthukumaran.ganapathy@nurturebox.ai>
2024-01-09 22:05:04 +0530

    Initial version of app

---
+++ challenge2/challenge_persistentTimer_gmkumaran87/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+
+import "./App.css";
+import Header from "./components/Header";
+import SynonymFinder from "./pages/SynonymFinder";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
